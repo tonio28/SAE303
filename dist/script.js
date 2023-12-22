240916582b2a9f63298770eced87ec40598ea184
antoine chauveau <a.chauveau87@gmail.com>
2023-12-22 13:09:48 +0100

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,123 +1,187 @@
-gsap.registerPlugin(ScrollTrigger);
+// LottieInteractivity.create({
+//   mode: "scroll",
+//   player: "#LottiePlay1",
+//   actions: [
+//     {
+//       visibility: [0, 1],
+//       type: "play",
+//     },
+//   ],
+// });
 
-let sections = gsap.utils.toArray(".panel");
+// LottieInteractivity.create({
+//   mode: "scroll",
+//   player: "#LottiePlay2",
+//   actions: [
+//     {
+//       visibility: [0, 1],
+//       type: "play",
+//     },
+//   ],
+// });
 
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".container",
-    pin: true,
-    scrub: true,
-    end: () => "+=" + document.querySelector(".container").offsetWidth,
-  },
+// LottieInteractivity.create({
+//   mode: "scroll",
+//   player: "#LottiePlay3",
+//   actions: [
+//     {
+//       visibility: [0, 1],
+//       type: "play",
+//     },
+//   ],
+// });
+
+LottieInteractivity.create({
+  mode: "scroll",
+  player: "#LottiePlay4",
+  actions: [
+    {
+      visibility: [0.4, 0.42],
+      type: "play",
+    },
+  ],
+});
+LottieInteractivity.create({
+  mode: "scroll",
+  player: "#LottiePlay5",
+  actions: [
+    {
+      visibility: [0.3, 0.32],
+      type: "play",
+    },
+  ],
+});
+LottieInteractivity.create({
+  mode: "scroll",
+  player: "#LottiePlay6",
+  actions: [
+    {
+      visibility: [0.3, 0.32],
+      type: "play",
+    },
+  ],
 });
 
-let player = document.getElementById("firstLottie");
-
-player.addEventListener("ready", () => {
-  LottieInteractivity.create({
-    mode: "scroll",
-    player: "#firstLottie",
-    actions: [
-      {
-        visibility: [0.3, 0.7],
-        type: "seek",
-        frames: [0, 300],
-      },
-    ],
-  });
+LottieInteractivity.create({
+  mode: "scroll",
+  player: "#firstLottie",
+  actions: [
+    {
+      visibility: [0.3, 0.7],
+      type: "seek",
+      frames: [0, 300],
+    },
+  ],
 });
+
+function createLottieInteractivity(elementId) {
+  var animationElement = document.getElementById(elementId);
+
+  if (animationElement) {
+    // Vérifiez si l'élément avec l'ID existe avant de créer l'interaction
+    LottieInteractivity.create({
+      player: "#" + elementId,
+      mode: "scroll",
+      actions: [
+        {
+          visibility: [0.3, 0.51],
+          type: "play",
+        },
+      ],
+    });
+  }
+}
+createLottieInteractivity("LottiePlay7");
+createLottieInteractivity("LottiePlay8");
+createLottieInteractivity("LottiePlay9");
 
 function setupScrollAnimation(targetSelector) {
-  var targetElement = document.querySelector(targetSelector);
-  var animationExecuted = false;
-  var scrollHeight = parseInt(targetElement.dataset.scrollHeight, 10) || 0;
-  var initialPosition;
-
   // Définissez différentes configurations d'animation.
   var animationConfigurations = {
-    Animation9999: {
-      targets: targetSelector + " input",
-      value: [0, 9999],
-      round: 1,
+    AnimationLeft: {
+      targets: targetSelector,
       easing: "easeInOutExpo",
       autoplay: false,
-    },
-    Animation500: {
-      targets: targetSelector + " input",
-      value: [0, 500],
-      round: 1,
-      easing: "easeInOutExpo",
-      autoplay: false,
+      translateX: 250,
     },
 
-    Animation100: {
-      targets: targetSelector + " input",
-      value: [0, 100],
-      round: 1,
+    AnimationRight: {
+      targets: targetSelector,
       easing: "easeInOutExpo",
       autoplay: false,
-      targets: targetSelector + " input",
-      translateX: 250,
-      translateY: 250,
+      translateX: -250,
     },
-    AnimationBidon: {
-      targets: targetSelector + " input",
-      easing: "easeInOutExpo",
-      autoplay: false,
-      translateX: 250,
-      translateY: 250,
+
+    Animation30: {
+      targets: "#animation30", // Use the correct ID
+      innerHTML: [0, 30],
+      easing: "linear",
+      round: 1,
+      update: function (anim) {
+        document.querySelector(targetSelector).textContent =
+          anim.animations[0].currentValue + "%";
+      },
+    },
+    Animation43: {
+      targets: "#animation43", // Use the correct ID
+      innerHTML: [0, 43],
+      easing: "linear",
+      round: 1,
+      update: function (anim) {
+        document.querySelector(targetSelector).textContent =
+          anim.animations[0].currentValue + "%";
+      },
+    },
+    Animation74: {
+      targets: "#animation74", // Use the correct ID
+      innerHTML: [0, 74],
+      easing: "linear",
+      round: 1,
+      update: function (anim) {
+        document.querySelector(targetSelector).textContent =
+          anim.animations[0].currentValue + "%";
+      },
+    },
+    Animation84: {
+      targets: "#animation84", // Use the correct ID
+      innerHTML: [0, 84],
+      easing: "linear",
+      round: 1,
+      update: function (anim) {
+        document.querySelector(targetSelector).textContent =
+          anim.animations[0].currentValue + "%";
+      },
+    },
+    Animation45: {
+      targets: "#animation45", // Use the correct ID
+      innerHTML: [0, 45],
+      easing: "linear",
+      round: 1,
+      update: function (anim) {
+        document.querySelector(targetSelector).textContent =
+          anim.animations[0].currentValue + "%";
+      },
     },
   };
 
-  // Ajoutez un écouteur d'événements pour le défilement de la fenêtre.
+  var targetElement = document.querySelector(targetSelector);
+
+  // Check if the target element exists before proceeding
+  if (!targetElement) {
+    console.error("Target element not found for selector:", targetSelector);
+    return;
+  }
+
+  var animationExecuted = false;
+  var scrollHeight = parseInt(targetElement.dataset.scrollHeight, 10) || 0;
+  var initialPosition = anime.get(
+    targetSelector + " input",
+    "translateX",
+    "translateY"
+  );
 
   window.addEventListener("scroll", function () {
-    // Vérifiez si l'utilisateur a fait défiler jusqu'à l'élément spécifié et si l'animation n'a pas encore été exécutée.
     if (window.scrollY > scrollHeight && !animationExecuted) {
       targetElement.classList.remove("hidden");
 
-      var animationName = targetElement.dataset.animationName || "default";
-      var animationConfig = animationConfigurations[animationName];
-
-      // Si la configuration d'animation est définie, jouez l'animation.
-      if (animationConfig) {
-        initialPosition = anime.get(
-          targetSelector + " input",
-          "translateX",
-          "translateY"
-        );
-        anime(animationConfig).play();
-      } else {
-        console.log("Type d'animation non reconnu");
-      }
-
-      // Marquez que l'animation a été exécutée.
-      animationExecuted = true;
-    } else if (window.scrollY <= scrollHeight && animationExecuted) {
-      // Ajoutez la classe "hidden" pour masquer l'élément lorsque l'utilisateur fait défiler vers le haut.
-      targetElement.classList.add("hidden");
-
-      // Arrêtez l'animation lorsque l'utilisateur fait défiler vers le haut.
-      // Notez que le type d'animation doit être récupéré à nouveau ici pour être utilisé correctement.
-      var animationName = targetElement.dataset.animationName || "default";
-      anime(animationConfigurations[animationName]).pause();
-
-      anime.set(targetSelector + " input", { translateX: initialPosition });
-      anime.set(targetSelector + " input", { translateY: initialPosition });
-
-      // Marquez que l'animation n'est plus en cours d'exécution.
-      animationExecuted = false;
-    }
-  });
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Appel à la fonction une fois que le DOM est prêt
-  // Utilisez la fonction avec le sélecteur spécifique.
-  setupScrollAnimation("#animation");
-  setupScrollAnimation("#animation2");
-  setupScrollAnimation("#animation3");
-  setupScrollAnimation("#animation4");
-});
+      var
